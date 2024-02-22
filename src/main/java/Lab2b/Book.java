55ce5cab00e5b0a7aa3404c6180debcfc2a833ec
KOK <yungkangkok@gmail.com>
2024-02-22 12:01:28 +0800

    lab2

---
+++ src/main/java/Lab2b/Book.java
@@ -0,0 +1,29 @@
+package Lab2b;
+
+public class Book {
+    private String chapters[];
+    private static final int DEFAULT_CHAPTERS = 10;
+
+    public Book() {
+        chapters = new String[DEFAULT_CHAPTERS];
+        for (int i = 0; i < chapters.length; i++)
+            chapters[i] = "n/a";
+    }
+    public Book(String argument[]) {
+        /* construct the object with an array of chapter names */
+        chapters = new String[DEFAULT_CHAPTERS];
+        for(int i = 0;i<argument.length;i++)
+            chapters[i] = argument[i];
+        for(int j =argument.length;j<chapters.length;j++)
+            chapters[j] = "n/a";
+        /* PLEASE ADD YOUR CODE HERE */
+    }
+    public String getChapter(int i) {
+        /* return the chapter by the given index */
+        return chapters[i];
+        /* PLEASE ADD YOUR CODE HERE */
+    }
+    public String[] getChapters() {
+        return chapters;
+    }
+}
